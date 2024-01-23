1490b7a0a86858d00422bad76316fa835b33421a
librasvii <moreiralucas511@gmail.com>
2024-01-23 17:48:40 -0300

    decreasing insertion-short update

--- insert_short/1/sort_number_descreasing.c
+++ insert_short/1/sort_number_descreasing.c
@@ -2,19 +2,28 @@
 
 int main() {
   int array[] = {5, 2, 4, 6, 1, 3};
-  int n;
+  int n = sizeof(array) / sizeof(array[0]);
 
-  for (int i = 0; i < array[n]; i++) {
-    int key = array[i];
-    int j = 1 + i;
-    while ((j > 0) && (array[j] > key)) {
-      array[j - 1] = array[j];
-      j = j + 1;
+  for (int i = 1; i < n; i++) {
+    // printf("\nA[] => %d\n", array[i]);
+    int key = array[i]; // take each value from array
+    printf("\nkey => %d\n", key);
+    int j = i - 1; // take the value from the loop for
+    printf("\ni => %d\n", i);
+    printf("\nj => %d\n", j);
+    printf("\nA[j] => %d\n", array[j]);
+    while (j > 0 && array[j] < key) {
+      printf("inside while");
+      array[j + 1] = array[j]; // move the value inside array
+      printf("\nWhile A[j] => %d\n", array[j]);
+      printf("\nwhile j => %d\n", j);
+      j = j - 1;
+      printf("\nbelow while j => %d\n", j);
     }
-    array[j - 1] = key;
+    array[j + 1] = key;
   }
 
-  for (int i = 0; i < array[n]; i++) {
+  for (int i = 0; i < n; i++) {
     printf("%d ", array[i]);
   }
 
